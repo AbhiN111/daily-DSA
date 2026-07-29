751d2973f45ebe659428320d351009c3a1206f8f
AbhiN111 <aknawale7@gmail.com>
2026-07-29 19:30:55 +0530

    Added Q3 optimal solution in arrayHard.java

--- arrayHard.java
+++ arrayHard.java
@@ -134,6 +134,39 @@ public class arrayHard {
         return new ArrayList<>(st);
     }
 
+    //Q3.3 sum(optimal)
+    public static List<List<Integer>> threeSum2(int arr[], int n){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        for (int i = 0; i<n ;i++){
+            if(i>0 && arr[i] == arr[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = arr[i] +arr[j]+arr[k];
+
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(arr[i]);
+                    temp.add(arr[j]);
+                    temp.add(arr[k]);
+
+                    ans.add(temp);
+                    while (j < k && arr[j] == arr[j - 1])
+                        j++;
+
+                    while (j < k && arr[k] == arr[k + 1])
+                        k--;
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
@@ -180,9 +213,15 @@ public class arrayHard {
         // System.out.println("Majority Elements (> n/3 times): " + ans2);
 
         //Q3.Call to 3 sum(better)
-        List<List<Integer>> ans = threeSum(arr, n);
+        // List<List<Integer>> ans1 = threeSum(arr, n);
+        // System.out.println("Triplets are:");
+        // for (List<Integer> list : ans1) {
+        //     System.out.println(list);
+        // }
+        //Q3.Call to 3 sum(optimal)
+        List<List<Integer>> ans2 = threeSum(arr, n);
         System.out.println("Triplets are:");
-        for (List<Integer> list : ans) {
+        for (List<Integer> list : ans2) {
             System.out.println(list);
         }
 
