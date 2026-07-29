b5f808d1314ba0f34ac24788a58e696bf7789eed
AbhiN111 <aknawale7@gmail.com>
2026-07-29 13:13:10 +0530

    Added Q2 optimal solution to arrayHard.java

--- arrayHard.java
+++ arrayHard.java
@@ -67,6 +67,50 @@ public class arrayHard {
         return ans;
     }
 
+    //Q2.Majority Element-II(optimal)
+     public static List<Integer> majorityElement2(int[] nums, int n) {
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && nums[i] != ele2) {
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if (cnt2 == 0 && nums[i] != ele1) {
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if (nums[i] == ele1) {
+                cnt1++;
+            }
+            else if (nums[i] == ele2) {
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == ele1)
+                cnt1++;
+            else if (nums[i] == ele2)
+                cnt2++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        int min = (n / 3) + 1;
+
+        if (cnt1 >= min)
+            ans.add(ele1);
+        if (cnt2 >= min)
+            ans.add(ele2);
+        return ans;
+    }
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
@@ -107,8 +151,11 @@ public class arrayHard {
         // }
 
         //Q2.Call to Majority Element-II(better)
-        List<Integer> anss = majorityElement(arr, n);
-        System.out.println("Majority Elements (> n/3 times): " + anss);
+        List<Integer> ans1 = majorityElement(arr, n);
+        System.out.println("Majority Elements (> n/3 times): " + ans1);
+        //Q2.Call to Majority Element-II(optimal)
+        List<Integer> ans2 = majorityElement(arr, n);
+        System.out.println("Majority Elements (> n/3 times): " + ans2);
 
     }
 }
