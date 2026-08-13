a790784dac8f76f4f0e22f1fdc3e3746d5861501
AbhiN111 <aknawale7@gmail.com>
2026-08-13 10:34:11 +0530

    Added Q3 to BSonANS.java

--- BSonANS.java
+++ BSonANS.java
@@ -53,19 +53,65 @@ public class BSonANS {
         return 0;
     }
 
+    //Q3.Koko eating bananas
+    public static int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;
+        int n = v.length;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
+        }
+        return maxi;
+    }
+    //Q3 function 2
+    public static int calculateTotalHours(int[] v, int hourly) {
+        int totalH = 0;
+        for (int i = 0; i < v.length; i++) {
+            totalH += Math.ceil((double) v[i] / hourly);
+        }
+        return totalH;
+    }
+    //Q3 function 3
+    public static int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = findMax(piles);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(piles, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number(n): ");
-        int n = sc.nextInt();
+        // System.out.print("Enter a number(n): ");
+        // int n = sc.nextInt();
 
-        System.out.print("Enter a number(m): ");
-        int m = sc.nextInt();
+        // System.out.print("Enter a number(m): ");
+        // int m = sc.nextInt();
         
         // //Q1.Call to Find square root of a number
         // System.out.println("Square root of " + n + " = " + findSquareRoot(n));
 
-        //Q2.Call to Find Nth root of a number
-        System.out.println("Nth root = " + NthRoot(n, m));
+        // //Q2.Call to Find Nth root of a number
+        // System.out.println("Nth root = " + NthRoot(n, m));
+
+        //Q3.Call to Koko eating bananas
+        System.out.print("Enter number of piles: ");
+        int n = sc.nextInt();
+        int[] piles = new int[n];
+        System.out.println("Enter bananas in each pile:");
+        for (int i = 0; i < n; i++) {
+            piles[i] = sc.nextInt();
+        }
+        System.out.print("Enter maximum hours: ");
+        int h = sc.nextInt();
+        System.out.println("Minimum eating speed = " + minEatingSpeed(piles, h));
     }
 }
