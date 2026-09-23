89083540e38b845118e5c39c74e199399a545aa0
AbhiN111 <aknawale7@gmail.com>
2026-09-23 09:58:47 +0530

    Added Q1 approach2 in array2D.java

--- array2D.java
+++ array2D.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class array2D {
+public class array2D{
 
     //Q1.Find row with maximum 1's
     public static int rowWithMinimumOne1(int arr[][], int n, int m){
@@ -19,6 +19,39 @@ public class array2D {
         return ind;
     }
 
+    //Q1. Find row with maximum 1's - function 1
+    public static int lowerBound(int arr[], int n, int x) {
+        int low=0, high=n - 1;
+        int ans=n;
+        while(low <= high) {
+            int mid= (low +high) / 2;
+            // maybe an answer
+            if(arr[mid] >= x) {
+                ans = mid;
+                // look for more small index on left
+                high = mid -1;
+            }
+            else {
+                low =mid+1; // look for right
+            }
+        }
+        return ans;
+    }
+
+    //Q1.function 2
+    public static int rowWithMinimumOne2(int arr[][], int n, int m){
+        int cnt_max = 0;
+        int index = -1;
+        for(int i = 0 ;i<n;i++) {
+            int cnt_ones = m - lowerBound(arr[i], m, 1);
+            if(cnt_ones >cnt_max) {
+            cnt_max =cnt_ones;
+            index = i;
+            }
+        }
+        return index;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
@@ -33,7 +66,9 @@ public class array2D {
             }
         }
 
-        //Q1.Call to Find row with maximum 1's
+        //Q1.Call to Find row with maximum 1's - approach 1
         System.out.println("Row with maximum number of 1s: " + rowWithMinimumOne1(arr, n, m));
+        //Q1.Call to Find row with maximum 1's - approach 2
+        System.out.println("Row with maximum number of 1s: " + rowWithMinimumOne2(arr, n, m));
     }
 }
